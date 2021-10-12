617527db593f48ef987115ebc40bd736ccd1905c
Brandon <brandon121j@gmail.com>
2021-10-12 15:05:49 -0400

    Completion of comments model

--- routes/comments/model/Comments.js
+++ routes/comments/model/Comments.js
@@ -1,12 +1,20 @@
 const mongoose = require('mongoose');
 
 commentsSchema = new mongoose.Schema (
-    {
-        comment: {
+    {   
+        originalPoster: {
             type: String
         },
+        user: {
+            type: String
+        },
+        comment: {
+            type: String
+        }
     },
     {
         timestamps: true
     }
 );
+
+module.exports = mongoose.model('comments', commentsSchema);
